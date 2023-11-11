7967cc8fde1aa87c785674dcf5105d7ac4252040
andreafrancioni <andreafrancioni.work@gmail.com>
2023-11-11 15:33:57 +0100

    Creato main project, importato asset e array di oggetti

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const team = [
+    {
+        image: "wayne-barnett-founder-ceo.jpg",
+        name: "Wayne Barnett",
+        role: "Founder & CEO"
+    },
+    {
+        image: "angela-caroll-chief-editor.jpg",
+        name: "Angela Caroll",
+        role: "Chief Editor"
+    },
+    {
+        image: "walter-gordon-office-manager.jpg",
+        name: "Walter Gordon",
+        role: "Office Manager"
+    },
+    {
+        image: "angela-lopez-social-media-manager.jpg",
+        name: "Angela Lopez",
+        role: "Social Media Manager"
+    },
+    {
+        image: "scott-estrada-developer.jpg",
+        name: "Scott Estrada",
+        role: "Developer"
+    },
+    {
+        image: "barbara-ramos-graphic-designer.jpg",
+        name: "Barbara Ramos",
+        role: "Graphic Designer"
+    },
+];
